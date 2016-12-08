42fdfbc3a25323428934e8d0ce2be1c702dbed05
Raymond Lee <hoyenlee@yahoo.com>
2016-12-08 03:55:44 -0500

    update search component

--- app/scripts/hero-search.component.js
+++ app/scripts/hero-search.component.js
@@ -1,19 +1,20 @@
 (function(angular){
 "use strict";
-	function controller($scope,$document,$timeout,heroService){
+	function controller($scope,$document,$state,$timeout,heroService){
 		var ctrl = this;
-		ctrl.heroes = ctrl.heroes || [];
+		ctrl.heroes = [];
 		ctrl.searchTerm = '';
+		ctrl.showList = (typeof(ctrl.showList)!=='undefined') ? ctrl.showList : true;
 		var searchTimeout = null;
 		ctrl.search = function(){
 			clearTimeout(searchTimeout);
 			$timeout.cancel(searchTimeout);
 			searchTimeout = $timeout(function(){
+				console.log("ctrl.showList",ctrl.showList)
 				if(!ctrl.showList){
 					ctrl.onSearch({value: ctrl.searchTerm});
 				}
 				else{
-					
 					if(ctrl.searchTerm.trim() === ""){
 						ctrl.heroes = [];
 					}
@@ -31,14 +32,18 @@
 
 
 			},1000);
+		} // end ctrl.search
+
+		ctrl.gotoDetail = function(hero){
+			$state.go('hero-details', { id: hero.id });
 		}
 
-	}
+	} // end controller
 
 	angular.module('tourOfHeroesApp')
 	.component('heroSearch',{
 		templateUrl: '../templates/hero-search.html',
-		controller: ['$scope','$document','$timeout','heroService',controller],
+		controller: ['$scope','$document','$state','$timeout','heroService',controller],
 		bindings: {
 			'showList': '<',
 			'onSearch': '&'
